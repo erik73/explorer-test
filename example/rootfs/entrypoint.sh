57d64b645c40ce58011eeffb774c4125ad0a6170
Erik Hilton <erikhilton73@gmail.com>
2026-03-04 11:10:46 +0100

    Session key

--- example/rootfs/entrypoint.sh
+++ example/rootfs/entrypoint.sh
@@ -2,14 +2,14 @@
 # shellcheck disable=SC2181
 cp -n /db/sqlite.db /data/db/sqlite.db
 
-if [ -z "$SESSION_SECRET_KEY" ]; then
-    echo "SESSION_SECRET_KEY not found, generating random key..."
-    SESSION_SECRET_KEY=$(openssl rand -hex 32)
-    export SESSION_SECRET_KEY
-    echo "Generated SESSION_SECRET_KEY: ${SESSION_SECRET_KEY:0:8}..."
-else
-    echo "Using existing SESSION_SECRET_KEY: ${SESSION_SECRET_KEY:0:8}..."
-fi
+#if [ -z "$SESSION_SECRET_KEY" ]; then
+#    echo "SESSION_SECRET_KEY not found, generating random key..."
+#    SESSION_SECRET_KEY=$(openssl rand -hex 32)
+export SESSION_SECRET_KEY=ab9ed71fa7dbe51b6074db998c89212c06aeb18ed313113175b8e5bb25d417e4
+#    echo "Generated SESSION_SECRET_KEY: ${SESSION_SECRET_KEY:0:8}..."
+#else
+echo "Using existing SESSION_SECRET_KEY: ${SESSION_SECRET_KEY:0:8}..."
+#fi
 
 echo "SESSION_SECRET_KEY is set: ${SESSION_SECRET_KEY:0:8}..."
 
